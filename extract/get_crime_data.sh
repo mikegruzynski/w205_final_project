dff101c79e9d2c9a972f5ae4e193e6c462949ec9
Jay Zuniga <jay_zuniga@berkeley.edu>
2017-07-22 10:15:14 -0700

    Update get_crime_data.sh

--- extract/get_crime_data.sh
+++ extract/get_crime_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 export $CRIME_DIR = "$HOME/w205/crime"
-export $HDFS_CRIME_DIR = "/user/w205"
+export $HDFS_CRIME_DIR = "crime_data"
 
-# 
+# ensure directory exists
 mkdir -p $CRIME_DIR
 
 #Get Crime data file
 wget -O $CRIME_DIR/crime.csv https://data.seattle.gov/d/3k2p-39jp?category=Public-Safety&view_name=Seattle-Police-Department-911-Incident-Response
 
 #Create directory for hdfs
-hdfs dfs -mkdir $HDFS_CRIME_DIR/crime_data/
+hdfs dfs -mkdir $HDFS_CRIME_DIR
 
 # Uploads data files to HDFS, with each file having its specific directory
-hdfs dfs -put $CRIME_DIR/crime.csv $HDFS_CRIME_DIR/crime_data/
+hdfs dfs -put $CRIME_DIR/crime.csv $HDFS_CRIME_DIR
