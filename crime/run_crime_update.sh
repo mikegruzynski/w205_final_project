cd80bd92a5b8831cebd7b072f45652e31e65825a
Jay Zuniga <jay_zuniga@berkeley.edu>
2017-08-11 16:25:08 -0700

    Update run_crime_update.sh

--- crime/run_crime_update.sh
+++ crime/run_crime_update.sh
@@ -9,4 +9,4 @@ mkdir -p $CRIME_DIR 2>/dev/null
 wget -O $CRIME_DIR/SeattleCrimeData.csv https://data.seattle.gov/d/3k2p-39jp?category=Public-Safety&view_name=Seattle-Police-Department-911-Incident-Response
 
 #Run .sql script to update crime table in postgres
-psql -d $2 -u $3 create_postgres_crime.sql
+psql -d $2 -u $3 $CRIME_DIR/create_postgres_crime.sql
