c0c709faef89ede33635edfda035e4bc97e3f389
Jay Zuniga <jay_zuniga@berkeley.edu>
2017-08-11 20:20:33 -0700

    Update run_crime_update.sh
    Adds wait to ensure download finishes

--- crime/run_crime_update.sh
+++ crime/run_crime_update.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Usage: ./run_crime_update.sh <crime_files_directory> <database_name> <user/role>
-export CRIME_DIR = "$1"
+export CRIME_DIR="$1"
 
 # ensure directory exists, ignore error if it already exists
 mkdir -p $CRIME_DIR 2>/dev/null
 
 #Get Crime data file, rename it as SeattleCrimeData.csv
-wget -O $CRIME_DIR/SeattleCrimeData.csv https://data.seattle.gov/d/3k2p-39jp?category=Public-Safety&view_name=Seattle-Police-Department-911-Incident-Response
+wget -O /tmp/SeattleCrimeData.csv https://data.seattle.gov/d/3k2p-39jp?category=Public-Safety&view_name=Seattle-Police-Department-911-Incident-Response
+
+# Wait for download to finish
+wait
 
 #Run .sql script to update crime table in postgres
-psql -d $2 -u $3 $CRIME_DIR/create_postgres_crime.sql
+psql -d "$2" -U "$3" -f $CRIME_DIR/create_postgres_crime.sql
